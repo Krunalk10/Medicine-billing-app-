9ddd7063939212d6520357078b24e72d2322cd1f
Krunal <krunalkamble4@gmail.com>
2026-02-19 22:25:34 +0530

    Created login page

---
+++ app/components/login/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+	return (
+		<div className="loginForm">
+			<h1 className="text-4xl flex text-">Login</h1>
+			<input type="text" />
+			<input type="password" required />
+		</div>
+	);
+};
+
+export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Login from "./components/login/login";
 
 const Homepage = () => {
-	return <div className="text-4xl">Homepage</div>;
+	return (
+		<div className="h-screen flex">
+			<div className="flex-1 bg-lime-300">
+				<Login />
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
